69c1a7d9e1ba55d70efa774cd2434b71fc8d1040
cali-kennedy <calikennedy81@gmail.com>
2024-10-30 17:55:55 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(541, 146)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(143, 35)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(197, 95)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 35)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseWaitCursor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(115, 240)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 20)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Rcv:"
+        Me.Label2.UseWaitCursor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(115, 161)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 20)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Send:"
+        Me.Label1.UseWaitCursor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(197, 234)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(310, 26)
+        Me.TextBox2.TabIndex = 7
+        Me.TextBox2.UseWaitCursor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(197, 155)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(310, 26)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.UseWaitCursor = True
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Server"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿'Server Side
+'This code is for a simple TCP server application in Visual Basic .NET. It
+'creates a TCP server that listens on port 1000 for incoming connections.
+'Once a connection is established, it receives data from the client and displays
+'it in TextBox2. The user can also send data to the client by typing in
+'TextBox1 and clicking Button2.
+Imports System.Net
+Imports System.Net.Sockets
+
+Public Class Form1
+
+    'Creates a Socket
+    Dim TCPServer As Socket
+
+    'Listens for Connections from tcp servers
+    Dim TCPListenerz As TcpListener
+
+
+    Private Sub Button2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button2.Click
+        ' Converts the text from TextBox1 to ASCII bytes
+        Dim sendbytes() As Byte = System.Text.Encoding.ASCII.GetBytes(TextBox1.Text)
+        ' Sends the ASCII bytes through the TCPServer socket
+        TCPServer.Send(sendbytes)
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+
+        'Creates a TCP Listener that listens for any IP address with socket 1000
+        TCPListenerz = New TcpListener(IPAddress.Any, 1000)
+        ' Start Listener
+        TCPListenerz.Start()
+        'When a client sends a request, TCP accepts the client
+        TCPServer = TCPListenerz.AcceptSocket()
+        ' By default, when TCP is waiting to receive is stalls the programm
+        ' Turn that off
+        TCPServer.Blocking = False
+        ' Start timer to continuosly check for incoming data
+        Timer1.Enabled = True
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        ' Use a try block to avoid receiving errors when not receiving anything
+        Try
+            ' Create an array that's the size of ReceiveBufferSize
+            Dim rcvbytes(TCPServer.ReceiveBufferSize) As Byte
+            ' Receive data from TCPServer and store it in rcvbytes array
+            TCPServer.Receive(rcvbytes)
+            ' Convert received bytes to ASCII string and display in TextBox2
+            TextBox2.Text = System.Text.Encoding.ASCII.GetString(rcvbytes)
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
